a61cb27b06df9bf38ca8749262ef4439465e6e0b
ghdwlgns <atene1408@gmail.com>
2023-05-10 06:42:55 +0900

    refactor : 해당 Bean은 SecureConfig 파일로 이동함

--- src/main/java/com/chatkau/ChatKauApplication.java
+++ src/main/java/com/chatkau/ChatKauApplication.java
@@ -2,18 +2,11 @@ package com.chatkau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ChatKauApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatKauApplication.class, args);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
