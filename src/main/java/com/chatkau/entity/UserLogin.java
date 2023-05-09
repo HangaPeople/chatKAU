0d70d2f4ed6155b38e3b60b16aa05d643c4a8820
ghdwlgns <atene1408@gmail.com>
2023-05-10 06:39:44 +0900

    refactor : 로그인 시퀀스 변경에 따른 불필요한 프로퍼티 제거

--- src/main/java/com/chatkau/entity/UserLogin.java
+++ src/main/java/com/chatkau/entity/UserLogin.java
@@ -1,24 +1,17 @@
 package com.chatkau.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Getter
 @SuperBuilder
 @NoArgsConstructor
-public class UserLogin extends BaseEntity implements UserDetails {
+@AllArgsConstructor
+public class UserLogin extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -37,37 +30,4 @@ public class UserLogin extends BaseEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     @Column
     private UserRole role;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<GrantedAuthority> roles = new ArrayList<>();
-
-        roles.add(new SimpleGrantedAuthority(role.toString()));
-
-        return roles;
-    }
-
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
